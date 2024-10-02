e25b355ce2dbbd90761e3169e5418c08c2da5153
정찬욱 <jeongchan-ug@jeongchan-ug-ui-MacBookPro.local>
2024-10-02 16:11:56 +0900

    feat: 기본적인 hearder ui 생성

---
+++ app/ui/header/hearder.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { PopoverGroup } from "@headlessui/react";
+
+export default function Header() {
+  return (
+    <header className="bg-white">
+      <nav
+        aria-label="Global"
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+      >
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+          </a>
+        </div>
+
+        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+          <div className="flex items-center space-x-6">
+            <h1 className="text-sm font-semibold leading-6 text-gray-900">
+              푸쉬 알림 테스트
+            </h1>
+          </div>
+        </PopoverGroup>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end"></div>
+      </nav>
+    </header>
+  );
+}
