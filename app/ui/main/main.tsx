0fa5f2feed1dacfefa7521019ac2f40c4f756324
정찬욱 <jeongchan-ug@jeongchan-ug-ui-MacBookPro.local>
2024-10-07 18:30:47 +0900

    feat: width가 모바일 버전일때 텍스트가 보이게 처리

--- app/ui/main/main.tsx
+++ app/ui/main/main.tsx
@@ -9,14 +9,22 @@ export default function Main() {
     setCount(count + 1);
   };
   return (
-    <div className="my-8 flex justify-center items-center">
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={handleClick}
-      >
-        알림 전송
-      </button>
-      <p className="ml-6">알림 횟수 : {count}</p>
+    <div>
+      <div className="my-8 flex justify-center items-center">
+        <h1 className="md:hidden text-2xl font-semibold leading-6">
+          모바일 버전
+        </h1>
+      </div>
+
+      <div className="my-8 flex justify-center items-center">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleClick}
+        >
+          알림 전송
+        </button>
+        <p className="ml-6">알림 횟수 : {count}</p>
+      </div>
     </div>
   );
 }
